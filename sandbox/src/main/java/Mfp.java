c50d7b1e3a2cd3cc806c3915e05cc6d8a94ac9c3
Motionlessx <80837189+Motionlessx@users.noreply.github.com>
2022-07-02 00:27:30 +0300

    some methods added

--- sandbox/src/main/java/Mfp.java
+++ sandbox/src/main/java/Mfp.java
@@ -1,9 +1,24 @@
 public class Mfp {
     public static void main (String[] args) {
-        System.out.println("hello world!");
-    }
-    public static void hello () {
+        hello("world");
+        hello("user");
 
+        double x = 5;
+        System.out.println(bbs(x));
+
+        double a = 4;
+        double b = 6;
+        System.out.println(bbs(a, b));
+    }
+    public static void hello (String somebd) {
+        System.out.println("hello " + somebd);
+    }
+
+    public static double bbs (double l) {
+        return l * l;
+    }
+    public static double bbs (double a, double b) {
+        return a * b;
     }
 }
 
