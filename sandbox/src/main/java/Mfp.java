0f6720572187546a24743f026dcb43e82f5133b8
Motionlessx <medium83512@gmail.com>
2022-07-01 16:42:28 +0300

    gsdg

--- sandbox/src/main/java/Mfp.java
+++ sandbox/src/main/java/Mfp.java
@@ -1,7 +1,7 @@
 public class Mfp {
     public static void main (String[] args) {
 
-        System.out.println("hello fasd world!");
+        System.out.println("hello fasasdfasdfd world!");
     }
 }
 
