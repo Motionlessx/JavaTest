953151c8c8d980f93f4c1c01ca978dc632d4027f
Motionlessx <80837189+Motionlessx@users.noreply.github.com>
2022-07-08 12:53:42 +0300

    sssomeg

---
+++ sandbox/src/main/java/Second.java
@@ -0,0 +1,15 @@
+    public class Second {
+
+        private int m;
+        private int c;
+
+    void setInta(int am){
+      m = am;
+    }
+//    void setIntb(int b){
+//        this.c = c;
+//    }
+    void getInta() {
+        System.out.println(m);
+    }
+}
---
+++ sandbox/src/main/java/Start.java
@@ -0,0 +1,16 @@
+import java.util.Locale;
+
+public class Start {
+    public static void main(String[] args) {
+//        String str = "Hello";
+//        String str1 = "World";
+////        String str3 = str.toUpperCase() + str1;
+//        int str4 = str.length();
+//        System.out.println(str == str1);
+    }
+    public static void setParams(String[] args){
+        Second sec = new Second();
+        sec.setInta(5);
+        sec.getInta();
+    }
+}
