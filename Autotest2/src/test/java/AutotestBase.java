9d624dc3d5d8c218331599db2ac388be55887b8a
Motionlessx <80837189+Motionlessx@users.noreply.github.com>
2022-07-11 14:10:52 +0300

    first successful autotest

--- Autotest2/src/test/java/AutotestBase.java
+++ Autotest2/src/test/java/AutotestBase.java
@@ -11,13 +11,24 @@ public class AutotestBase {
 
 
 @Test
-    public void login() {
-    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    public void login() throws InterruptedException {
+//    driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     driver.manage().window().maximize();
     driver.get("https://cp-inst334-client.phonexa.xyz");
     driver.findElement(By.xpath("//*[@id=\"loginform-email\"]")).sendKeys("said@doubledmarketing.com");
     driver.findElement(By.xpath("//*[@id=\"loginform-password\"]")).sendKeys("12345Qwerty!");
     driver.findElement(By.xpath("//*[@id=\"login-form\"]/div[4]/div[2]/button")).click();
+//    driver.get("https://cp-inst334-client.phonexa.xyz/p8/dashboard");
+//
+//    //WebElement dynamicElement = login.driver.findElement(By.xpath("/html/body/div[1]/aside[1]/section/ul/li[4]/a"));
+//    Thread.sleep(10000);
+//    driver.findElement(By.xpath("/html/body/div[1]/aside[1]/section/ul/li[4]/a")).click();
+//    driver.findElement(By.xpath("/html/body/div[1]/aside[1]/section/ul/li[4]/ul/li[6]/a/span")).click();
+//    driver.findElement(By.xpath("/html/body/div[1]/div[4]/section[2]/div[3]/div/div/div[2]/div/div[1]/div[1]/button/span[2]")).click();
+//    driver.findElement(By.xpath("//*[@id=\"goalform-name\"]")).sendKeys("zxczxczxc");
+//    driver.findElement(By.xpath("//*[@id=\"goalform-conditions-0-visits-count\"]")).sendKeys("5");
+//    driver.findElement(By.xpath("//*[@id=\"goal-form\"]/div[5]/button[1]")).click();
+//    driver.findElement(By.xpath("//*[@id=\"modalPopup\"]/div/div/div[2]/div/div[2]/button")).click();
 }
 
 @Test
--- Autotest2/src/test/java/WebsiteTest.java
+++ Autotest2/src/test/java/WebsiteTest.java
@@ -1,6 +1,10 @@
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.sql.Driver;
 import java.util.concurrent.TimeUnit;
@@ -13,19 +17,24 @@ public class WebsiteTest {
     @Test
     public void testWebsite() throws InterruptedException {
         login.login();
-        login.driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        Thread.sleep(5000);
-        login.driver.get("https://cp-inst334-client.phonexa.xyz/p8/main");
-        login.driver.findElement(By.xpath("/html/body/div[1]/aside[1]/section/ul/li[4]/a/span")).click();
-        login.driver.findElement(By.xpath("/html/body/div[1]/aside[1]/section/ul/li[4]/ul/li[6]/a/span")).click();
-        login.driver.findElement(By.xpath("/html/body/div[1]/div[4]/section[2]/div[3]/div/div/div[2]/div/div[1]/div[1]/button/span[2]")).click();
+//        login.driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         Thread.sleep(10000);
+        login.driver.get("https://cp-inst334-client.phonexa.xyz/p8/dashboard");
+
+    //WebElement dynamicElement = login.driver.findElement(By.xpath("/html/body/div[1]/aside[1]/section/ul/li[4]/a"));
+        Thread.sleep(5000);
+        login.driver.findElement(By.xpath("/html/body/div[1]/aside[1]/section/ul/li[4]/a")).click();
+        Thread.sleep(2000);
+        login.driver.findElement(By.xpath("/html/body/div[1]/aside[1]/section/ul/li[4]/ul/li[6]/a/span")).click();
+        Thread.sleep(2000);
+        login.driver.findElement(By.xpath("/html/body/div[1]/div[4]/section[2]/div[3]/div/div/div[2]/div/div[1]/div[1]/button/span[2]")).click();
+        Thread.sleep(2000);
         login.driver.findElement(By.xpath("//*[@id=\"goalform-name\"]")).sendKeys("zxczxczxc");
         login.driver.findElement(By.xpath("//*[@id=\"goalform-conditions-0-visits-count\"]")).sendKeys("5");
         login.driver.findElement(By.xpath("//*[@id=\"goal-form\"]/div[5]/button[1]")).click();
-
-
-
-
+        Thread.sleep(2000);
+        login.driver.findElement(By.xpath("//*[@id=\"modalPopup\"]/div/div/div[2]/div/div[2]/button")).click();
+     //  Assert.assertFalse(login.driver.findElement(By.className("ellipsis tooltip-applied")).isDisplayed());
+//       Assert.assertEquals(login.driver.findElement());
     }
 }
