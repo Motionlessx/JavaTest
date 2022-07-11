7714a39b5a67dfdd08faf94f64be868e2e069812
Motionlessx <80837189+Motionlessx@users.noreply.github.com>
2022-07-11 15:30:10 +0300

    first successful autotest

---
+++ Autotest2/src/test/java/SegmentsCrud.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+public class SegmentsCrud {
+    AutotestBase login = new AutotestBase();
+    @Test
+    public void testSegmentsCrud() throws InterruptedException{
+        login.login();
+
+
+    }
+}
