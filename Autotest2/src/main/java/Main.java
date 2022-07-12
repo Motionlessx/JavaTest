48974d7217948f8274d9d0e83c2e4c35394a7451
Motionlessx <80837189+Motionlessx@users.noreply.github.com>
2022-07-13 02:21:39 +0300

    zxcs

--- Autotest2/src/main/java/Main.java
+++ Autotest2/src/main/java/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        int i;
+        int[] arr = new int[15];
+        for (i = 0; i < 15; i++) {
+            arr[i] = i * 1;
 
+        }
+        System.out.println(arr[arr.length - 1]);
     }
+
 }
