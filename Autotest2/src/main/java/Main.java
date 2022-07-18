b6dfe9d0856df61379fe8c25f5a61665b9d4d06b
Motionlessx <80837189+Motionlessx@users.noreply.github.com>
2022-07-19 00:08:54 +0300

    zxcsxas

--- Autotest2/src/main/java/Main.java
+++ Autotest2/src/main/java/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         int i;
@@ -7,6 +10,20 @@ public class Main {
 
         }
         System.out.println(arr[arr.length - 1]);
+
+        System.out.println();
+
+
+        List<Integer> list = new ArrayList<>();
+        list.add(5);
+        list.add(10);
+        list.add(20);
+        list.set(0, 200);
+        System.out.println(list.get(0));
+        list.remove(0);
+        System.out.println(list.size());
+
     }
+
 
 }
